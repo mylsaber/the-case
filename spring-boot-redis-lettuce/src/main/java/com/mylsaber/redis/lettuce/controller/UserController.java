f51c8437901970a8167fc400d685d80fa89a64d7
mylsaber <mylsaber@163.com>
2023-12-05 00:03:05 +0800

    lettuce客户端的基本用法

--- spring-boot-redis-lettuce/src/main/java/com/mylsaber/redis/lettuce/controller/UserController.java
+++ spring-boot-redis-lettuce/src/main/java/com/mylsaber/redis/lettuce/controller/UserController.java
@@ -2,13 +2,22 @@ package com.mylsaber.redis.lettuce.controller;
 
 import com.mylsaber.redis.lettuce.domain.ResponseResult;
 import com.mylsaber.redis.lettuce.dto.User;
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisFuture;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.async.RedisStringAsyncCommands;
+import io.lettuce.core.api.reactive.RedisStringReactiveCommands;
+import io.lettuce.core.api.sync.RedisStringCommands;
 import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
 
 /**
  * @author jiangdi
  */
+@Slf4j
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -26,6 +35,53 @@ public class UserController {
     @GetMapping("find/{userId}")
     public ResponseResult<User> find(@PathVariable("userId") String userId) {
         return ResponseResult.success(redisTemplate.opsForValue().get(userId));
+    }
+
+    @GetMapping("sync")
+    public ResponseResult<String> sync() {
+        try (RedisClient client = RedisClient.create("redis://127.0.0.1")) {
+            StatefulRedisConnection<String, String> connection = client.connect();
+            RedisStringCommands<String, String> sync = connection.sync();
+            String value = sync.get("key");
+            return ResponseResult.success(value);
+        } catch (Exception e) {
+            return ResponseResult.fail();
+        }
+    }
+
+    @GetMapping("async")
+    public ResponseResult<String> async() {
+        try (RedisClient client = RedisClient.create("redis://127.0.0.1")) {
+            StatefulRedisConnection<String, String> connection = client.connect();
+            RedisStringAsyncCommands<String, String> async = connection.async();
+            RedisFuture<String> set = async.set("key", "hellow");
+            RedisFuture<String> get = async.get("key");
+
+            log.info("set: {}, get: {}", set.get(), get.get());
+            return ResponseResult.success();
+
+        } catch (Exception e) {
+            log.error("async error", e);
+            return ResponseResult.fail();
+        }
+    }
+
+    @GetMapping("reactive")
+    public ResponseResult<String> reactive() {
+        try (RedisClient client = RedisClient.create("redis://127.0.0.1")) {
+            StatefulRedisConnection<String, String> connection = client.connect();
+            RedisStringReactiveCommands<String, String> reactive = connection.reactive();
+            Mono<String> set = reactive.set("key", "value");
+            Mono<String> get = reactive.get("key");
+
+            set.subscribe();
+            String block = get.block();
+            log.info("get: {}", block);
+            return ResponseResult.success();
+        } catch (Exception e) {
+            log.error("async error", e);
+            return ResponseResult.fail();
+        }
     }
 
 }
